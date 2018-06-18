156008187c212dcb61c1d42520fc1d865867a550
Rajat Gupta <rjtgupta84@gmail.com>
2018-06-18 16:35:56 +0530

    Add the packages

--- ubuntu-16.04_kernel.sh
+++ ubuntu-16.04_kernel.sh
@@ -1,1 +1,4 @@
 
+sudo apt update && sudo apt upgrade -y
+sudo apt install gcc g++ zip unzip tar make clang llvm git fakeroot build-essential ncurses-dev \
+xz-utils libssl-dev bc kmod cpio flex cpio libncurses5-dev python
