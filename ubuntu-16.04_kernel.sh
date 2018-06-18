78d8c15324a3eea0e931d4db78f23c5d20d571c1
Rajat Gupta <rjtgupta84@gmail.com>
2018-06-18 17:25:25 +0530

    Add ccache to ubuntu-18.04-aosp and 16.04-kernel.sh

--- ubuntu-16.04_kernel.sh
+++ ubuntu-16.04_kernel.sh
@@ -1,3 +1,3 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install gcc g++ zip unzip tar make clang llvm git fakeroot build-essential ncurses-dev \
+sudo apt install gcc g++ ccache zip unzip tar make clang llvm git fakeroot build-essential ncurses-dev \
 xz-utils libssl-dev bc kmod cpio flex cpio libncurses5-dev python
