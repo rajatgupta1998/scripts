5325d2de8a6aa719b7d640f2efb63f1a749547da
Aditya singh <adi.singh199@gmail.com>
2018-08-20 22:44:31 +0530

    ubuntu1604_kernel : login as su

--- ubuntu-16.04_kernel.sh
+++ ubuntu-16.04_kernel.sh
@@ -1,3 +1,4 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install gcc g++ ccache zip unzip tar make clang llvm git fakeroot build-essential ncurses-dev \
+sudo su
+apt update &&  apt upgrade -y
+apt install gcc g++ ccache zip unzip tar make clang llvm git fakeroot build-essential ncurses-dev \
 xz-utils libssl-dev bc kmod cpio flex cpio libncurses5-dev python -y
