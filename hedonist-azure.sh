a2b35ed7f3348ed3b9fd624fd8cbe34a8da54026
Rajat Gupta <rajatgupta1998@acm.org>
2019-09-03 23:25:13 +0530

    hedonist: Clean and update mr hedonist
    Signed-off-by: Rajat Gupta <rajatgupta1998@acm.org>

--- hedonist-azure.sh
+++ hedonist-azure.sh
@@ -1,1 +1,10 @@
 echo $whoami "welcome to HEDONIST"
+
+
+echo "OK Microsoft, I will always prefer a clean and up-to-date system"
+
+# cleans apt cache and removes useless packages
+sudo apt clean && sudo apt autoremove --purge
+
+# update apt repos and do a system update
+sudo apt update && sudo apt upgrade
