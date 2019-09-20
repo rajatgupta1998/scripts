ee99cd1f8d73aa7f67e620d543222407152dcbf5
Rajat Gupta <rajatgupta1998@acm.org>
2019-09-20 13:08:57 +0530

    hedonist: Also display the current OS' release number and the codename
    * lsb_release -r and -c. Also append a blank line
    
    Signed-off-by: Rajat Gupta <rajatgupta1998@acm.org>

--- hedonist-azure.sh
+++ hedonist-azure.sh
@@ -1,6 +1,10 @@
 LSB_RELEASE_D="$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
+LSB_RELEASE_R="$(lsb_release -r | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
+LSB_RELEASE_C="$(lsb_release -c | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
 
-echo $whoami "Welcome to HEDONIST, running on ""${LSB_RELEASE_D}" .
+echo $whoami "Welcome to HEDONIST, running on ""${LSB_RELEASE_D}" "${LSB_RELEASE_R}", "${LSB_RELEASE_C}".
+
+echo -e ""
 
 echo "OK Microsoft, I will always prefer a clean and up-to-date system"
 
