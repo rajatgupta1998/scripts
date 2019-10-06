a164f67c9e0c3e70a0832cfd1eb3b7d160a98815
Rajat Gupta <rajatgupta1998@acm.org>
2019-10-06 20:50:31 +0530

    Katzzero: I don't want to set python2 everytime

--- katzzero-arch.sh
+++ katzzero-arch.sh
@@ -43,4 +43,5 @@ yay -S maven gradle
 echo "Setting up python2 for AOSP"
 mdkir ~/bin
 ln -s /bin/python2 ~/bin/python
-export PATH=~/bin:$PATH
+echo "export PATH=~/bin:$PATH" >> ~/.bashrc
+. ~/.bashrc
