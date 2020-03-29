556e491103fdf3e5558bdc183742d96d2dc37c0b
Rajat Gupta <rjtgupta84@gmail.com>
2020-03-29 18:13:40 +0530

    bionic: Install Chrome

--- setup/ubuntu-18.04_devsetup.sh
+++ setup/ubuntu-18.04_devsetup.sh
@@ -6,3 +6,9 @@ pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev g++-multilib li
 ncurses-dev texinfo gcc gperf patch libtool automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcloog-isl-dev libcap-dev \
 autoconf libgmp-dev pkg-config libmpc-dev libmpfr-dev lzma* liblzma* w3m android-tools-* maven ncftp htop imagemagick \
 libssl-dev clang cmake axel gradle pngcrush lzip ccache pigz python-pip python3-pip neofetch openssh-server re2c libxml-simple-perl patchelf -y
+
+# Google Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt update
+sudo apt install google-chrome-stable
