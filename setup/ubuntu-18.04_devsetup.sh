6d31f1277b86ded2b02e562093389beb3b65877c
Rajat Gupta <rjtgupta84@gmail.com>
2020-03-29 17:45:50 +0530

    bionic: Install openssh-server
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- setup/ubuntu-18.04_devsetup.sh
+++ setup/ubuntu-18.04_devsetup.sh
@@ -5,7 +5,7 @@ sudo apt install curl git python gnupg flex bison gperf libsdl1.2-dev squashfs-t
 pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev g++-multilib lib32z1-dev lib32ncurses5-dev gcc-multilib liblz4-* pngquant \
 ncurses-dev texinfo gcc gperf patch libtool automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcloog-isl-dev libcap-dev \
 autoconf libgmp-dev build-essential pkg-config libmpc-dev libmpfr-dev lzma* liblzma* w3m android-tools-* maven ncftp htop imagemagick \
-libssl-dev clang cmake axel gradle pngcrush lzip ccache pigz python-pip python3-pip neofetch -y
+libssl-dev clang cmake axel gradle pngcrush lzip ccache pigz python-pip python3-pip neofetch openssh-server -y
 
 #flex errors bois
 echo "export LC_ALL=C" >> ~/.bashrc
