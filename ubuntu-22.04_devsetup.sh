391049818b0503c2a8ac3f5a62ee265495ddc27c
Rajat Gupta <rjtgupta84@gmail.com>
2022-07-07 11:19:31 +0530

    Update jdk to 11
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- ubuntu-22.04_devsetup.sh
+++ ubuntu-22.04_devsetup.sh
@@ -1,11 +1,11 @@
 # update your rusty system
 sudo apt update && sudo apt upgrade -y
 
-#sudo apt install curl git python2 gnupg flex bison gperf libsdl1.2-dev squashfs-tools build-essential zip libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk \
-#pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev g++-multilib lib32z1-dev lib32ncurses5-dev gcc-multilib liblz4-* pngquant \
-#ncurses-dev texinfo gcc gperf patch libtool automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcap-dev \
-#autoconf libgmp-dev pkg-config libmpc-dev libmpfr-dev lzma* liblzma* w3m maven ncftp htop imagemagick \
-#libssl-dev clang cmake axel gradle pngcrush lzip ccache pigz python-pip neofetch openssh-server re2c libxml-simple-perl patchelf tree screen -y
+sudo apt install curl git python2 gnupg flex bison gperf libsdl1.2-dev squashfs-tools build-essential zip libncurses5-dev zlib1g-dev openjdk-11-jdk \
+pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev g++-multilib lib32z1-dev lib32ncurses5-dev gcc-multilib liblz4-* pngquant \
+ncurses-dev texinfo gcc gperf patch libtool automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcap-dev \
+autoconf libgmp-dev pkg-config libmpc-dev libmpfr-dev lzma* liblzma* w3m maven ncftp htop imagemagick \
+libssl-dev clang cmake axel gradle pngcrush lzip ccache pigz python-pip neofetch openssh-server re2c libxml-simple-perl patchelf tree screen -y
 
 # Google Chrome
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
